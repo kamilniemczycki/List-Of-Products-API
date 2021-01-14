59fd146b4b45ea314833b49367ba2021513bbc67
Adrian Lidzbarski <adrianli102@gmail.com>
2021-01-14 18:19:46 +0100

    added login and registration to api router

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->name('user');
 });
+
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
