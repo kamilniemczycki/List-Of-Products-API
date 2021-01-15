ed35797a2f0c2a5b48283d4a2c1dee64a883f21a
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-01-15 12:42:52 +0100

    added all to api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ListController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductsAndListsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
@@ -20,6 +23,30 @@ Route::middleware('auth:api')->group(function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     })->name('user');
+
+    Route::name('lists.')->group(function() {
+        Route::get('/lists', [ListController::class, 'getLists'])->name('getLists');
+        Route::post('/lists', [ListController::class, 'createList'])->name('createList');
+        Route::get('/list/{id}', [ListController::class, 'listDetails'])->name('listDetails');
+        Route::put('/list/{id}', [ListController::class, 'editList'])->name('editList');
+        Route::delete('/list/{id}', [ListController::class, 'deleteList'])->name('deleteList');
+    });
+
+    Route::name('products.')->group(function() {
+        Route::get('/products', [ProductController::class, 'getProducts'])->name('getProducts');
+        Route::get('/product/{id}', [ProductController::class, 'getDetails'])->name('productDetails');
+    });
+
+    Route::name('list_products.')->group(function() {
+        Route::get('/list/{listId}/product/{productId}', [ProductsAndListsController::class, 'getDetails'])
+            ->name('getDetails');
+        Route::put('/list/{listId}/product/{productId}', [ProductsAndListsController::class, 'addProductToList'])
+            ->name('addProductToList');
+        Route::post('/list/{listId}/product/{productId}', [ProductsAndListsController::class, 'editProductOnList'])
+            ->name('editProductOnList');
+        Route::delete('/list/{listId}/product/{productId}', [ProductsAndListsController::class, 'removeProductFromList'])
+            ->name('removeProductFromList');
+    });
 });
 
 Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
